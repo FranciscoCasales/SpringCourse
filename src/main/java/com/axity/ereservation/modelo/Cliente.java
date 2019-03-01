7fb18dbfdd6f163a3c616f844f9d13c4735e24c3
Jose Francisco Casales Huerta <jose.casales@outlook.com>
2019-02-28 23:52:23 -0600

    Se mapearon los ORM

---
+++ src/main/java/com/axity/ereservation/modelo/Cliente.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.axity.ereservation.modelo;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+/**
+ * @author francisco
+ *
+ */
+@Entity
+@Table(name = "cliente")
+@Data
+public class Cliente {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid2")
+	private String idCli;
+	private String nombreCli;
+	private String apellidoCli;
+	private String identificacionCli;
+	private String direccionCli;
+	private String telefonoCli;
+	private String emailCli;
+	@OneToMany(mappedBy="cliente")
+	private Set<Reserva> reservas;
+	
+	public Cliente() {
+		
+	}
+}
